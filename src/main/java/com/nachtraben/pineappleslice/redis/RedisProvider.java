1a83e791f39a3de1163296de28e2a17ba66db712
NachtRaben <stilleraben@gmail.com>
2017-06-20 23:22:29 -0400

    Add exceptions to interface

--- src/main/java/com/nachtraben/pineappleslice/redis/RedisProvider.java
+++ src/main/java/com/nachtraben/pineappleslice/redis/RedisProvider.java
@@ -1,17 +1,18 @@
 package com.nachtraben.pineappleslice.redis;
 
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisException;
 
 /**
  * Created by NachtRaben on 4/20/2017.
  */
 public interface RedisProvider {
 
-        Redis getSession(int index);
+        Redis getSession(int index) throws JedisException;
 
-        void subscribe(String channel, RedisSubscriber subscriber) throws SubscriberException;
+        void subscribe(String channel, RedisSubscriber subscriber) throws SubscriberException, JedisException;
 
-        void psubscribe(String pattern, RedisSubscriber subscriber) throws SubscriberException;
+        void psubscribe(String pattern, RedisSubscriber subscriber) throws SubscriberException, JedisException;
 
         void unSubscribe(RedisSubscriber subscriber);
 
